d7c8e5abc7c6f2adafacd84a78cc9f5f6d63b483
s.vazhorov <leonvsg@gmail.com>
2018-05-16 23:50:42 +0300

    бд в azure

--- src/main/java/ru/leonvsg/education/timesheet/Settings.java
+++ src/main/java/ru/leonvsg/education/timesheet/Settings.java
@@ -5,8 +5,8 @@ public class Settings {
     private Settings() { }
 
     public static final String DB_DRIVER_NAME = "org.postgresql.Driver";
-    public static final String DB_CONNECTION_URL = "jdbc:postgresql://localhost:5432/education";
-    public static final String DB_CONNECTION_USERNAME = "timesheet";
+    public static final String DB_CONNECTION_URL = "jdbc:postgresql://educationpsqldb.postgres.database.azure.com:5432/education";
+    public static final String DB_CONNECTION_USERNAME = "timesheet@educationpsqldb";
     public static final String DB_CONNECTION_PASSWORD = "timesheet";
     public static final String VALID_LOGIN_REGEX = "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$";
 }
