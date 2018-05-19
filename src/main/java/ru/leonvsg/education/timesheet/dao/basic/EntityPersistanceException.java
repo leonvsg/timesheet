cc4bb385d750455ce9b280f530545fbd284d64a7
s.vazhorov <leonvsg@gmail.com>
2018-05-19 10:48:48 +0300

    рефакторинг

--- src/main/java/ru/leonvsg/education/timesheet/dao/basic/EntityPersistanceException.java
+++ src/main/java/ru/leonvsg/education/timesheet/dao/basic/EntityPersistanceException.java
@@ -1,8 +1,6 @@
 package ru.leonvsg.education.timesheet.dao.basic;
 
-import java.sql.SQLException;
-
-public class EntityPersistanceException extends SQLException {
+public class EntityPersistanceException extends RuntimeException {
 
     public EntityPersistanceException(String message) {
         super(message);
