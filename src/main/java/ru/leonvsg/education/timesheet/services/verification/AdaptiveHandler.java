c87b97aa8d0f0ed4b2e13d5b8c758db4c84ba762
vazhorov <leonvsg@gmail.com>
2018-05-21 15:40:15 +0300

    Handler вынесен в абстрактный класс.

---
+++ src/main/java/ru/leonvsg/education/timesheet/services/verification/AdaptiveHandler.java
@@ -0,0 +1,14 @@
+package ru.leonvsg.education.timesheet.services.verification;
+
+public class AdaptiveHandler extends Handler {
+
+    private Checker checker;
+
+    public AdaptiveHandler(Checker checker){
+        this.checker = checker;
+    }
+
+    public boolean handle() {
+        return checker.check() && checkNext();
+    }
+}
